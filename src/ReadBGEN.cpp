b277da2fed8ebe1c331d518edf9c46e4b2179511
pancong419 <44169272+pancong419@users.noreply.github.com>
2023-08-16 14:08:35 -0500

    Update ReadBGEN.cpp

--- src/ReadBGEN.cpp
+++ src/ReadBGEN.cpp
@@ -1512,7 +1512,7 @@ void gemBGEN(int thread_num, double sigma2, double* resid, double* XinvXTX, vect
                 }
 
                 if (expSq == 0) {
-                    
+                    /*
                     // ZGStR
                     double* subZGStR = new double[Sq1];
                     subMatrix(ZGStR, subZGStR, Sq1, 1, Sq1, Sq1, i * Sq1);
@@ -1531,7 +1531,7 @@ void gemBGEN(int thread_num, double sigma2, double* resid, double* XinvXTX, vect
                     double* ZGSR2tZGSxinvZGStZGS = new double[Sq1 * Sq1];
                     matNmatNprod(subZGSR2tZGS, invZGStZGS[i], ZGSR2tZGSxinvZGStZGS, Sq1, Sq1, Sq1);
                     VarBetaAll[i] = new double[Sq1 * Sq1];
-                    matNmatNprod(invZGStZGS[i], ZGSR2tZGSxinvZGStZGS, VarBetaAll[i], Sq1, Sq1, Sq1);
+                    matNmatNprod(invZGStZGS[i], ZGSR2tZGSxinvZGStZGS, VarBetaAll[i], Sq1, Sq1, Sq1);*/
 
                     if (printMeta || printFull) {
 
